19e3b86d0af0a83c5224f34159489289b49886b0
soroosh-gb <soroosh.gb@gmail.com>
2020-09-17 16:59:40 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollectionDiv = document.querySelector("#toy-collection");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +13,38 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  
+
+  const getToys = () => {
+    
+    fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(toys => toys.forEach(renderToys))
+    
+  } 
+
+  const renderToys = toy => {
+    let toyNew = toy 
+    let toyCard = document.createElement('div')
+    toyCard.className = 'card'
+    toyCard.innerHTML = `
+    <img src=${toyNew.image} class="toy-avatar" />
+    <h2>${toyNew.name}</h2>
+  
+    <p>${toyNew.likes}</p>
+    <button class="like-btn">Like</button>
+    `
+
+    toyCollectionDiv.append(toyCard)
+  }
+
+
+
+
+
+
+
+ getToys()
 });
+
